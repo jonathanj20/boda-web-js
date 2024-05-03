36ead14241933566928edfa16c412abcfac08723
jonathanj20 <jonathanpossada@gmail.com>
2024-05-03 16:36:22 -0700

    Se puede navegar en la galería en pantallas grandes

--- src/gallery.js
+++ src/gallery.js
@@ -2,11 +2,18 @@ const imgGallery = document.getElementsByClassName('img-gallery');
 const fullImg = document.getElementById('fullImg');
 const fullImageContainer = document.getElementById('fullImageContainer');
 const btnCloseFullImage = document.getElementById('btnCloseFullImage');
+const backButton = document.getElementById('backButton');
+const advanceButton = document.getElementById('advanceButton');
+
+let indexImages = [];
+let currentIndexImage = 0;
 
 for (let img of imgGallery) {
     img.addEventListener("click", () => {
         showFullImage(img.src);
+        getCurrentImage();
     });
+
 }
 
 function showFullImage(src) {
@@ -16,4 +23,35 @@ function showFullImage(src) {
 
 btnCloseFullImage.addEventListener("click", () => {
     fullImageContainer.classList.add('hidden');
+});
+
+//obtengo la imagen actual
+function getCurrentImage() {
+    for (let i = 0; i < imgGallery.length; i++) {
+        if (imgGallery[i].src === fullImg.src) {
+            currentIndexImage = i;
+        }
+    }
+}
+
+/*cuando le doy al boton de avanzar, el indice de
+la imagen actual aumenta y se llama a la función showFullImage
+con el src de la imagen actual*/
+backButton.addEventListener("click", () => {
+    if (currentIndexImage > 0) {
+        currentIndexImage--;
+    }
+
+    showFullImage(imgGallery[currentIndexImage].src);
+});
+
+/*cuando le doy al boton de atr+as, el indice de
+la imagen actual se resta y se llama a la función showFullImage
+con el src de la imagen actual*/
+advanceButton.addEventListener("click", () => {
+    if (currentIndexImage < 7) {
+        currentIndexImage++;
+    }
+
+    showFullImage(imgGallery[currentIndexImage].src);
 });
--- src/index.php
+++ src/index.php
@@ -124,6 +124,18 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path>
           </svg>
         </button>
+        <div class="hidden md:flex items-center justify-between">
+          <button class="border-2 border-black hover:border-white hover:text-white" id="backButton">
+            <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+            </svg>
+          </button>
+          <button class="border-2 border-black hover:border-white hover:text-white" id="advanceButton">
+            <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+            </svg>
+          </button>
+        </div>
         <img alt="" class="md:mx-auto md:h-96 md:w-96" id="fullImg" />
       </div>
     </section>
