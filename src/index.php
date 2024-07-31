ff681dce713750eca9eebe4c58a815170f4535f3
jonathanj20 <jonathanpossada@gmail.com>
2024-07-31 13:47:14 -0700

    Se hizo la sección de ubicación y se modificaron estilos

--- src/index.php
+++ src/index.php
@@ -14,12 +14,12 @@ include 'connection_bd.php'
 
 <body class="transition-opacity">
   <header class="p-2 flex justify-between items-center md:flex-row-reverse md:w-8/12 md:mx-auto">
-    <button class="border-2 p-1 border-black md:hidden" id="btnOpen">
+    <button class="border-8 p-1 border-black md:hidden" id="btnOpen">
       <svg class="h-8 w-8" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
       </svg>
     </button>
-    <button id="btnClose" class="relative hidden border-2 border-black">
+    <button id="btnClose" class="relative hidden border-8 border-black">
       <svg class="h-10 w-10" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path>
       </svg>
@@ -34,7 +34,7 @@ include 'connection_bd.php'
     </nav>
     <img src="../images/logoboda.png" alt="logo" class="hover:rotate-12">
   </header>
-  <main class="bg-[url('../images/background2.jpg')] bg-cover bg-no-repeat">
+  <main class="">
     <nav class="bg-violet-300 py-5 w-screen relative top-0 left-0 hidden shadow-[0_h-full_0_0_rgba(0,0,0,0.5)]" id="nav">
       <ul class="space-y-6 flex flex-col text-center">
         <li><a href="#outfit" class="link-nav hover:text-white transition-all menu-link">Vestimenta</a></li>
@@ -53,7 +53,7 @@ include 'connection_bd.php'
           </div>
         </form>
       </div>
-      <div class="border-4 border-yellow-500 bg-white w-8/12 mx-auto space-y-4 p-3 text-center font-mono">
+      <div class="border-4 border-amber-200 bg-white w-8/12 mx-auto space-y-4 p-3 text-center font-mono">
         <h2 class="uppercase -tracking-tight">invitación para</h2>
         <?php
         if (isset($_POST['btnAceptar'])) {
@@ -90,7 +90,7 @@ include 'connection_bd.php'
     <section>
       <audio src="../audio/musica_prueba_boda.mp3" controls class="w-full bg-fuchsia-400"></audio>
     </section>
-    <section class="p-7 w-10/12 bg-white mx-auto mt-5 border-4 border-yellow-500 font-serif">
+    <section class="p-7 w-10/12 bg-white mx-auto mt-5 border-8 border-amber-200 font-serif">
       <div>
         <p class="text-center text-lg">Boda de</p>
         <p class="text-center font-bold text-4xl">Mercedes & Victor</p>
@@ -121,7 +121,7 @@ include 'connection_bd.php'
         </div>
       </div>
     </section>
-    <section class="p-7 w-10/12 bg-white mx-auto mt-5 border-4 border-yellow-500 font-serif text-center" id="outfit">
+    <section class="p-7 w-10/12 bg-white mx-auto mt-5 border-8 border-amber-200 font-serif text-center" id="outfit">
       <div class="h-14 mb-5 sm:mb-15">
         <h2>Código de vestimenta</h2>
         <img src="../images/separator2.png" alt="separator" class="mx-auto relative bottom-14">
@@ -130,40 +130,40 @@ include 'connection_bd.php'
         <div>
           <img src="../images/vestido.png" alt="vestido para mujer" class="mx-auto">
           <p>Para mujeres</p>
-          <p>Vestido largo</p>
+          <p>Vestido o traje formal</p>
         </div>
         <div>
           <img src="../images/traje.png" alt="traje para hombre" class="mx-auto">
           <p>Para hombres</p>
-          <p>Traje formal</p>
+          <p>Traje formal o camisa</p>
         </div>
       </div>
     </section>
     <section class="bg-[url('../images/background1.jpg')] bg-cover bg-no-repeat p-5 mt-5" id="gallery">
       <h2 class="text-center my-5">Galería de fotos</h2>
       <div class="space-y-7 flex items-center flex-col bg-white py-10 md:space-y-0 md:grid md:place-items-center md:grid-cols-2 md:p-10 md:gap-5 lg:grid-cols-4">
-        <img src="../images/fondo_8.jpg" alt="" class="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
-        <img src="../images/fondo_10.png" alt="" class="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
-        <img src="../images/fondo_11.png" alt="" class="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
-        <img src="../images/fondo2.jpeg" alt="" class="w-9/12  md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
-        <img src="../images/fondo4.jpeg" alt="" class="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
-        <img src="../images/novios_prueba.png" alt="" class="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
-        <img src="../images/background1.jpg" alt="" class="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
-        <img src="../images/background2.jpg" alt="" class="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
+        <img src="../images/fondo_8.jpg" alt="" class="w-9/12 md:w-72 md:h-72 border-8 border-amber-200 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
+        <img src="../images/fondo_10.png" alt="" class="w-9/12 md:w-72 md:h-72 border-8 border-amber-200 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
+        <img src="../images/fondo_11.png" alt="" class="w-9/12 md:w-72 md:h-72 border-8 border-amber-200 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
+        <img src="../images/fondo2.jpeg" alt="" class="w-9/12  md:w-72 md:h-72 border-8 border-amber-200 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
+        <img src="../images/fondo4.jpeg" alt="" class="w-9/12 md:w-72 md:h-72 border-8 border-amber-200 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
+        <img src="../images/novios_prueba.png" alt="" class="w-9/12 md:w-72 md:h-72 border-8 border-amber-200 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
+        <img src="../images/background1.jpg" alt="" class="w-9/12 md:w-72 md:h-72 border-8 border-amber-200 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
+        <img src="../images/background2.jpg" alt="" class="w-9/12 md:w-72 md:h-72 border-8 border-amber-200 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
       </div>
       <div class="hidden bg-black bg-opacity-30 p-5 fixed left-0 right-0 bottom-0 top-0 mx-auto" id="fullImageContainer">
-        <button class="border-2 border-black hover:border-white hover:text-white" id="btnCloseFullImage">
+        <button class="border-8 border-black hover:border-white hover:text-white" id="btnCloseFullImage">
           <svg class="h-10 w-10" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path>
           </svg>
         </button>
         <div class="hidden md:flex items-center justify-between">
-          <button class="border-2 border-black hover:border-white hover:text-white" id="backButton">
+          <button class="border-8 border-black hover:border-white hover:text-white" id="backButton">
             <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
             </svg>
           </button>
-          <button class="border-2 border-black hover:border-white hover:text-white" id="advanceButton">
+          <button class="border-8 border-black hover:border-white hover:text-white" id="advanceButton">
             <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
             </svg>
@@ -173,27 +173,27 @@ include 'connection_bd.php'
         <p class="text-center font-bold" id="numberImage"></p>
       </div>
     </section>
-    <section class="text-center">
-      <h2>Ubicaciones y horarios</h2>
+    <section class="text-center bg-[url('../images/copasvino3.png')] bg-cover bg-no-repeat bg-center">
+      <h2 class="text-white font-bold">Ubicaciones y horarios</h2>
       <img src="../images/separator2.png" alt="separador" class="mx-auto relative bottom-14">
-      <div class="space-y-16 md:flex md:space-y-0 my-auto">
-        <div class="border-4 p-5 border-purple-500 bg-white w-10/12 mx-auto md:w-2/6">
+      <div class="space-y-16 md:flex md:space-y-0 my-auto md:items-center">
+        <div class="border-8 p-5 border-amber-200 bg-slate-50  w-10/12 mx-auto md:w-2/6 h-50">
           <p class="mb-5">Ceremonia religiosa</p>
-          <img src="../images/church.png" alt="Iglesia" class="mb-5 mx-auto w-10/12 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer">
-          <p>Nombre de la iglesia</p>
+          <img src="../images/iglesia.jpg" alt="Iglesia" class="mb-5 mx-auto md:w-72 md:h-72 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer">
+          <p>Igleisa Betlehem</p>
           <p>6:00 PM</p>
           <img src="../images/separator2.png" alt="separador" class="mx-auto">
-          <p>Calle Ejemplo #33, colonia Ejemplo, Ciudad Ejemplo, Estado Ejemplo, CP: 00000</p>
-          <button class="border-2 border-purple-600 bg-purple-300 p-3 mt-3 text-white">Ver en GPS</button>
+          <p>Padre kino, 16 de Septiembre esq, Guerrero, 23020 La Paz, B.C.S.</p>
+          <a class="block border-2 border-purple-600 bg-purple-300 p-3 mt-3 text-white" href="https://www.google.com/maps/dir/24.144887,-110.297585/iglesia+bethlehem+ubicacion+la+paz/@24.1467437,-110.3005277,17z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x86afd3ef2462056f:0x5c00fbf878a18bab!2m2!1d-110.2987049!2d24.1486938?entry=ttu" target="_blank">Ver en GPS</a>
         </div>
-        <div class="border-4 p-5 border-purple-500 bg-white w-10/12 mx-auto md:w-2/6">
+        <div class="border-8 p-5 border-amber-200 bg-slate-50 w-10/12 mx-auto md:w-2/6 object-cover">
           <p class="mb-5">Recepción</p>
-          <img src="../images/church.png" alt="Iglesia" class="mb-5 mx-auto w-10/12 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer">
-          <p>Nombre del lugar</p>
+          <img src="../images/sotres2.jpg" alt="Iglesia" class="mb-5 mx-auto md:w-72 md:h-72 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer">
+          <p>Salón Sotres</p>
           <p>6:00 PM</p>
           <img src="../images/separator2.png" alt="separador" class="mx-auto">
-          <p>Calle Ejemplo #33, colonia Ejemplo, Ciudad Ejemplo, Estado Ejemplo, CP: 00000</p>
-          <button class="border-2 border-purple-600 bg-purple-300 p-3 mt-3 text-white">Ver en GPS</button>
+          <p>Pichilingue 19, Los Tabachines, 23048 La Paz, B.C.S.</p>
+          <a class="block border-2 border-purple-600 bg-purple-300 p-3 mt-3 text-white" href="https://www.google.com/maps/dir//Pichilingue+19,+Los+Tabachines,+23048+La+Paz,+B.C.S./@24.0679118,-110.3860395,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x86afcd66e6c76e9f:0x38a840618f577975!2m2!1d-110.3036379!2d24.0679339?entry=ttu" target="_blank">Ver en GPS</a>
         </div>
       </div>
     </section>
