531be215c14b39f0daaffce04ece1398af7d67ea
jonathanj20 <jonathanpossada@gmail.com>
2024-08-01 15:34:06 -0700

    Las fotos se pueden hacer grandes al dar clic

--- src/index.php
+++ src/index.php
@@ -12,14 +12,14 @@ include 'connection_bd.php'
   <link rel="stylesheet" href="../dist/styles.css">
 </head>
 
-<body class="transition-opacity">
+<body>
   <header class="p-2 flex justify-between items-center md:flex-row-reverse md:w-8/12 md:mx-auto">
-    <button class="border-8 p-1 border-black md:hidden" id="btnOpen">
+    <button class="border-2 p-1 border-black md:hidden" id="btnOpen">
       <svg class="h-8 w-8" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
       </svg>
     </button>
-    <button id="btnClose" class="relative hidden border-8 border-black">
+    <button id="btnClose" class="relative hidden border-2 border-black">
       <svg class="h-10 w-10" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path>
       </svg>
@@ -28,7 +28,7 @@ include 'connection_bd.php'
       <ul class="flex space-x-6">
         <li><a href="#outfit" class="hover:text-violet-600 menu-link">Vestimenta</a></li>
         <li><a href="#gallery" class="hover:text-violet-600 menu-link">Galería</a></li>
-        <li><a href="#" class="hover:text-violet-600 menu-link">Ubicación</a></li>
+        <li><a href="#location" class="hover:text-violet-600 menu-link">Ubicación</a></li>
         <li><a href="#" class="hover:text-violet-600 menu-link">Contacto</a></li>
       </ul>
     </nav>
@@ -39,7 +39,7 @@ include 'connection_bd.php'
       <ul class="space-y-6 flex flex-col text-center">
         <li><a href="#outfit" class="link-nav hover:text-white transition-all menu-link">Vestimenta</a></li>
         <li><a href="#gallery" class="link-nav hover:text-white transition-all menu-link">Galería</a></li>
-        <li><a href="#" class="link-nav hover:text-white transition-all menu-link">Ubicación</a></li>
+        <li><a href="#location" class="link-nav hover:text-white transition-all menu-link">Ubicación</a></li>
         <li><a href="#" class="link-nav hover:text-white transition-all menu-link">Contacto</a></li>
       </ul>
     </nav>
@@ -53,7 +53,7 @@ include 'connection_bd.php'
           </div>
         </form>
       </div>
-      <div class="border-4 border-amber-200 bg-white w-8/12 mx-auto space-y-4 p-3 text-center font-mono">
+      <div class="border-8 border-double border-amber-200 bg-white w-7/12 mx-auto space-y-4 p-3 bg-[url('../images/marcopase2.jpg')] bg-cover bg-no-repeat text-center font-mono">
         <h2 class="uppercase -tracking-tight">invitación para</h2>
         <?php
         if (isset($_POST['btnAceptar'])) {
@@ -90,13 +90,13 @@ include 'connection_bd.php'
     <section>
       <audio src="../audio/musica_prueba_boda.mp3" controls class="w-full bg-fuchsia-400"></audio>
     </section>
-    <section class="p-7 w-10/12 bg-white mx-auto mt-5 border-8 border-amber-200 font-serif">
+    <section class="p-7 w-10/12 mx-auto mt-5 border-8 border-amber-200 font-serif">
       <div>
         <p class="text-center text-lg">Boda de</p>
         <p class="text-center font-bold text-4xl">Mercedes & Victor</p>
       </div>
       <div>
-        <img src="../images/novios_prueba.png" alt="novios" class="w-60 mx-auto mt-8 rounded-full transition-all ease-in-out delay-75 hover:scale-110">
+        <img src="../images/novios_prueba.png" alt="novios" class="w-60 mx-auto mt-8 rounded-full transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img">
       </div>
       <div class="mt-5">
         <p class="text-center">Te esperámos el</p>
@@ -152,18 +152,18 @@ include 'connection_bd.php'
         <img src="../images/background2.jpg" alt="" class="w-9/12 md:w-72 md:h-72 border-8 border-amber-200 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img-gallery" />
       </div>
       <div class="hidden bg-black bg-opacity-30 p-5 fixed left-0 right-0 bottom-0 top-0 mx-auto" id="fullImageContainer">
-        <button class="border-8 border-black hover:border-white hover:text-white" id="btnCloseFullImage">
+        <button class="border-2 border-black hover:border-white hover:text-white" id="btnCloseFullImage">
           <svg class="h-10 w-10" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path>
           </svg>
         </button>
         <div class="hidden md:flex items-center justify-between">
-          <button class="border-8 border-black hover:border-white hover:text-white" id="backButton">
+          <button class="border-2 border-black hover:border-white hover:text-white" id="backButton">
             <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
             </svg>
           </button>
-          <button class="border-8 border-black hover:border-white hover:text-white" id="advanceButton">
+          <button class="border- border-black hover:border-white hover:text-white" id="advanceButton">
             <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
             </svg>
@@ -173,22 +173,22 @@ include 'connection_bd.php'
         <p class="text-center font-bold" id="numberImage"></p>
       </div>
     </section>
-    <section class="text-center bg-[url('../images/copasvino3.png')] bg-cover bg-no-repeat bg-center">
+    <section class="text-center bg-[url('../images/copasvino3.png')] bg-cover bg-no-repeat bg-center" id="location">
       <h2 class="text-white font-bold">Ubicaciones y horarios</h2>
       <img src="../images/separator2.png" alt="separador" class="mx-auto relative bottom-14">
       <div class="space-y-16 md:flex md:space-y-0 my-auto md:items-center">
         <div class="border-8 p-5 border-amber-200 bg-slate-50  w-10/12 mx-auto md:w-2/6 h-50">
           <p class="mb-5">Ceremonia religiosa</p>
-          <img src="../images/iglesia.jpg" alt="Iglesia" class="mb-5 mx-auto md:w-72 md:h-72 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer">
+          <img src="../images/iglesia.jpg" alt="Iglesia" class="mb-5 mx-auto md:w-72 md:h-72 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img">
           <p>Igleisa Betlehem</p>
           <p>6:00 PM</p>
           <img src="../images/separator2.png" alt="separador" class="mx-auto">
           <p>Padre kino, 16 de Septiembre esq, Guerrero, 23020 La Paz, B.C.S.</p>
           <a class="block border-2 border-purple-600 bg-purple-300 p-3 mt-3 text-white" href="https://www.google.com/maps/dir/24.144887,-110.297585/iglesia+bethlehem+ubicacion+la+paz/@24.1467437,-110.3005277,17z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x86afd3ef2462056f:0x5c00fbf878a18bab!2m2!1d-110.2987049!2d24.1486938?entry=ttu" target="_blank">Ver en GPS</a>
         </div>
-        <div class="border-8 p-5 border-amber-200 bg-slate-50 w-10/12 mx-auto md:w-2/6 object-cover">
+        <div class="border-8 p-5 border-amber-200 bg-slate-50 w-10/12 mx-auto md:w-2/6">
           <p class="mb-5">Recepción</p>
-          <img src="../images/sotres2.jpg" alt="Iglesia" class="mb-5 mx-auto md:w-72 md:h-72 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer">
+          <img src="../images/sotres2.jpg" alt="Iglesia" class="mb-5 mx-auto md:w-72 md:h-72 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer img">
           <p>Salón Sotres</p>
           <p>6:00 PM</p>
           <img src="../images/separator2.png" alt="separador" class="mx-auto">
@@ -197,9 +197,18 @@ include 'connection_bd.php'
         </div>
       </div>
     </section>
+    <div class="hidden bg-black bg-opacity-30 p-5 fixed left-0 right-0 bottom-0 top-0 mx-auto" id="containerFullImg">
+      <button class="border-2 border-black hover:border-white hover:text-white" id="btnCloseNormalImg">
+        <svg class="h-10 w-10" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path>
+        </svg>
+      </button>
+      <img class="md:mx-auto md:h-96 md:w-96" id="fullNormalImg" />
+    </div>
   </main>
   <script src="menu.js"></script>
   <script src="gallery.js"></script>
+  <script src="photos.js"></script>
 </body>
 
 </html>
---
+++ src/photos.js
@@ -0,0 +1,17 @@
+const photos = Array.from(document.getElementsByClassName('img'));
+const container = document.getElementById('containerFullImg');
+const photo = document.getElementById('fullNormalImg');
+const btnCloseNormalImg = document.getElementById('btnCloseNormalImg');
+
+photos.forEach((item) => {
+    item.addEventListener("click", () => {
+        container.classList.remove('hidden');
+        photo.src = item.src;
+    })
+});
+
+btnCloseNormalImg.addEventListener("click", () => {
+    container.classList.add('hidden');
+});
+
+
