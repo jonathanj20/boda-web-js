6aafb75d1be1dd1c51f5b29b79c0d667a6636219
jonathanj20 <jonathanpossada@gmail.com>
2024-09-18 09:45:26 -0700

    prueba

--- src/connection_bd.php
+++ src/connection_bd.php
@@ -4,10 +4,12 @@ $DB_USER = $_ENV['DB_USER'];
 $DB_PASSWORD = $_ENV['DB_PASSWORD'];
 $DB_NAME = $_ENV['DB_NAME'];
 $DB_PORT = $_ENV['DB_PORT'];
-$connection = mysqli_connect("$DB_HOST", "$DB_HOST", "$PASSWORD", "$DB_NAME", "$DB_PORT");
 
-/*if ($connection) {
-    echo "Base de datos conectada";
-} else {
-    echo "No se pudo conectar la base de datos";
-}*/
+$connection = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
+
+// Verificar si la conexión fue exitosa
+// if ($connection) {
+//     echo "Base de datos conectada";
+// } else {
+//     die("No se pudo conectar a la base de datos: " . mysqli_connect_error());
+// }
