30e7d5752732cafd1a05e1cf56b86fa397537529
jonathanj20 <jonathanpossada@gmail.com>
2024-09-18 09:55:59 -0700

    prueba

--- src/connection_bd.php
+++ src/connection_bd.php
@@ -5,7 +5,7 @@ $DB_PASSWORD = $_ENV['DB_PASSWORD'];
 $DB_NAME = $_ENV['DB_NAME'];
 $DB_PORT = $_ENV['DB_PORT'];
 
-$connection = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
+$connection = mysqli_connect("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
 
 // Verificar si la conexión fue exitosa
 // if ($connection) {
