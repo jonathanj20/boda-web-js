8fac29c288abfb54b4ded5fac4ccbb01c110ad67
jonathanj20 <jonathanpossada@gmail.com>
2024-09-17 21:50:51 -0700

    se restableció

--- src/connection_bd.php
+++ src/connection_bd.php
@@ -1,9 +1,8 @@
 <?php
-session_start();
-$DB_HOST = $_ENV["DB_HOST"];
-$DB_USER = $_ENV["DB_USER"];
-$DB_PASSWORD = $_ENV["DB_PASSWORD"];
-$DB_NAME = $_ENV["DB_NAME"];
-$DB_PORT = $_ENV["DB_PORT"];
+$connection = mysqli_connect("localhost", "root", "", "wedding");
 
-$connection = mysqli_connect("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
+/*if ($connection) {
+    echo "Base de datos conectada";
+} else {
+    echo "No se pudo conectar la base de datos";
+}*/
