7ac5e5b993a6a98b2ee1e1a3b525e4a83848e96a
jonathanj20 <jonathanpossada@gmail.com>
2024-09-18 09:36:33 -0700

    prueba

--- src/connection_bd.php
+++ src/connection_bd.php
@@ -1,5 +1,10 @@
 <?php
-$connection = mysqli_connect("localhost", "root", "", "wedding");
+$DB_HOST = $_ENV['DB_HOST'];
+$DB_USER = $_ENV['DB_USER'];
+$DB_PASSWORD = $_ENV['DB_PASSWORD'];
+$DB_NAME = $_ENV['DB_NAME'];
+$DB_PORT = $_ENV['DB_PORT'];
+$connection = mysqli_connect("$DB_HOST", "$DB_HOST", "$PASSWORD", "$DB_NAME", "$DB_PORT");
 
 /*if ($connection) {
     echo "Base de datos conectada";
